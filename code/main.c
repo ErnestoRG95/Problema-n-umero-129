2aa67c91140a69bdcde908cb948e025f781ea5c8
Anna Rodriguez <annarodriguez@iMac-de-Anna.local>
2018-12-09 18:26:51 +0100

    constantes y recorrido de argumentos

--- code/main.c
+++ code/main.c
@@ -1,6 +1,17 @@
 #include<stdio.h>
 #include<string.h>
 
+#define CERO {1,1,1,0,1,1,1}
+#define UNO 2
+#define DOS 5
+#define TRES 5
+#define CUATRO 4
+#define CINCO 5
+#define SEIS 6
+#define SIETE 3
+#define OCHO 7
+#define NUEVE 6
+
 int entradaValida(int nArgumentos, char **argumentos){
 	int valido = 1;
 	if(!strcmp(*(argumentos+nArgumentos-1), "-1")){
@@ -18,7 +29,14 @@ int entradaValida(int nArgumentos, char **argumentos){
 
 
 int main(int argc,char **argv){
-	entradaValida(argc-1, argv+1);
+	int solucion = 0;
+	if(entradaValida(argc-1, argv+1)){
+		for (int i = 1; i < argc-1; ++i)
+		{
+
+		}
+		printf("%d\n", solucion);
+	}
 
 	
 
