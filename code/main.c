8518fab172cd3e62bcc18114e584bc5c1c4e597f
Anna Rodriguez <annarodriguez@iMac-de-Anna.local>
2018-12-10 22:39:00 +0100

    inicializacion del panel y transformacion de la entrada

--- code/main.c
+++ code/main.c
@@ -1,6 +1,10 @@
 #include<stdio.h>
+#include<stdlib.h>
 #include<string.h>
 
+#define SEGMENTOS 7
+
+#define BASE 	{0,0,0,0,0,0,0}
 #define CERO	{1,1,1,0,1,1,1}
 #define UNO 	{0,0,0,0,0,1,1}
 #define DOS 	{1,0,1,1,1,0,1}
@@ -26,16 +30,44 @@ int entradaValida(int nArgumentos, char **argumentos){
 	return valido;
 }
 
+int** inizializarPanel(int digitos){
+	int **panel = (int **) malloc(sizeof(*panel)*digitos);
+	for (int i = 0; i < digitos; ++i)
+	{
+		int *fila = (int *) malloc(sizeof(**panel)*SEGMENTOS);
+		for (int j = 0; j < SEGMENTOS; ++j)
+		{
+			*(fila+j) = 0;
+		}
+		*(panel+i) = fila;
+		
+
+	}
+	
+	return panel;
+} 
+int* transformarEntrada(int nArgumentos, char **argumentos){
+	int *digitos = (int *) malloc(sizeof(int)*nArgumentos);
+	for (int i = 0; i < nArgumentos; ++i)
+	{
+		*(digitos+i) = **(argumentos+i)-'0';
+	}
+	return digitos;
+}
+	
+
+
+
 
 
 int main(int argc,char **argv){
 	int solucion = 0;
+	int **panel;
+	int *entrada;
 	if(entradaValida(argc-1, argv+1)){
-		for (int i = 1; i < argc-1; ++i)
-		{
-
-		}
-		printf("%d\n", solucion);
+		panel = inizializarPanel(argc-2);
+		entrada = transformarEntrada(argc-2, argv+1);
+		
 	}
 
 	
